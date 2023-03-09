22a303848815e8aa808712086b3e820ca044c3de
Hyohwak Lee <crewe1746@naver.com>
2023-03-09 20:34:48 +0900

    Run stub Authentication server

---
+++ cmd/auth/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/piatoss3612/tx-noti-bot/internal/app/rest"
+	"github.com/piatoss3612/tx-noti-bot/internal/logger"
+)
+
+var (
+	NAME = "Authentication"
+	PORT = "3000"
+)
+
+func main() {
+	logger.SetStructuredLogger(NAME, os.Stdout)
+
+	app, err := rest.New(NAME, PORT)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	shutdown, err := app.Setup().Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer app.Close()
+
+	<-shutdown
+}
