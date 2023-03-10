fc863962d9f287bf96a0ca0a576b4a18f60c08da
Hyohwak Lee <crewe1746@naver.com>
2023-03-10 23:51:34 +0900

    Implement user register and delete methods

--- internal/handler/auth/user.go
+++ internal/handler/auth/user.go
@@ -1,21 +1,65 @@
 package auth
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/piatoss3612/tx-noti-bot/internal/helpers"
+	"github.com/piatoss3612/tx-noti-bot/internal/models"
+)
 
 func (a *authHandler) RegisterUser(w http.ResponseWriter, r *http.Request) {
 	// read payload
+	var payload models.UserPayload
+
+	err := helpers.ReadJSON(w, r, &payload)
+	if err != nil {
+		helpers.ErrorJSON(w, http.StatusBadRequest, "") // TODO: error message
+		return
+	}
 
 	// create user data
+	var user models.User
+
+	user.ID = payload.ID
+	user.Email = payload.Email
+	user.DiscordID = payload.DiscordID
 
 	// insert user data to DB
+	err = a.repo.CreateUser(r.Context(), &user)
+	if err != nil {
+		helpers.ErrorJSON(w, http.StatusBadRequest, "")
+		return
+	}
 
 	// write response
+	var resp models.CommonResponse
+
+	resp.StatusCode = http.StatusOK
+
+	helpers.WriteJSON(w, http.StatusOK, resp)
 }
 
 func (a *authHandler) DeleteUser(w http.ResponseWriter, r *http.Request) {
 	// read payload
+	var payload models.UserPayload
+
+	err := helpers.ReadJSON(w, r, &payload)
+	if err != nil {
+		helpers.ErrorJSON(w, http.StatusBadRequest, "")
+		return
+	}
 
 	// delete user data from DB by user id
+	err = a.repo.DeleteUser(r.Context(), payload.ID)
+	if err != nil {
+		helpers.ErrorJSON(w, http.StatusBadRequest, "")
+		return
+	}
 
 	// write response
+	var resp models.CommonResponse
+
+	resp.StatusCode = http.StatusOK
+
+	helpers.WriteJSON(w, http.StatusOK, resp)
 }
