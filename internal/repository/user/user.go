43cd4761523db1baeb1eab573f6c607a4c4b459e
Hyohwak Lee <crewe1746@naver.com>
2023-03-10 12:51:52 +0900

    Define UserRepository interface

---
+++ internal/repository/user/user.go
@@ -0,0 +1,15 @@
+package user
+
+import (
+	"context"
+
+	"github.com/piatoss3612/tx-noti-bot/internal/models"
+)
+
+type UserRepository interface {
+	CreateUser(ctx context.Context, u *models.User) error
+	GetAllUsers(ctx context.Context) ([]*models.User, error)
+	GetUserByID(ctx context.Context, id string) (*models.User, error)
+	UpdateUser(ctx context.Context, u *models.User) error
+	DeleteUser(ctx context.Context, id string) error
+}
