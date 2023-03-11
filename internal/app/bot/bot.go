e3919df5098cf4a2d18915decd26f985c45b65b6
Hyohwak Lee <crewe1746@naver.com>
2023-03-11 15:09:56 +0900

    Remove lifecycle descriptions from bot and rest

--- internal/app/bot/bot.go
+++ internal/app/bot/bot.go
@@ -1,14 +1,12 @@
 package bot
 
 import (
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/piatoss3612/tx-noti-bot/internal/app"
-	"golang.org/x/exp/slog"
 )
 
 type bot struct {
@@ -22,29 +20,21 @@ func New(name, token string) (app.App, error) {
 		return nil, err
 	}
 
-	return &bot{name, session}, nil
+	return &bot{name: name, session: session}, nil
 }
 
 func (b *bot) Setup() app.App {
-	slog.Info(fmt.Sprintf("Start setup process of %s", b.name))
-
 	b.session.AddHandler(ping)
 	b.session.Identify.Intents = discordgo.IntentGuilds | discordgo.IntentGuildMessages
-
-	slog.Info(fmt.Sprintf("%s is now ready to start!", b.name))
 
 	return b
 }
 
 func (b *bot) Open() (<-chan bool, error) {
-	slog.Info("Open connection to discord server...")
-
 	err := b.session.Open()
 	if err != nil {
 		return nil, err
 	}
-
-	slog.Info(fmt.Sprintf("%s is now running!", b.name))
 
 	shutdown := make(chan bool)
 
@@ -66,11 +56,5 @@ func (b *bot) Open() (<-chan bool, error) {
 }
 
 func (b *bot) Close() error {
-	slog.Info(fmt.Sprintf("Close connection of %s...", b.name))
-
-	_ = b.session.Close()
-
-	slog.Info("Done closing process!")
-
-	return nil
+	return b.session.Close()
 }
--- internal/app/rest/rest.go
+++ internal/app/rest/rest.go
@@ -13,7 +13,6 @@ import (
 
 	"github.com/piatoss3612/tx-noti-bot/internal/app"
 	"github.com/piatoss3612/tx-noti-bot/internal/routes"
-	"golang.org/x/exp/slog"
 )
 
 var (
@@ -22,7 +21,6 @@ var (
 )
 
 type rest struct {
-	// TODO: appropriate fields
 	name string
 	port string
 	rc   routes.RouteController
@@ -47,21 +45,15 @@ func New(name, port string, rc routes.RouteController) (app.App, error) {
 }
 
 func (r *rest) Setup() app.App {
-	slog.Info(fmt.Sprintf("Start setup %s server", r.name))
-
 	r.srv = &http.Server{
 		Addr:    fmt.Sprintf(":%s", r.port),
 		Handler: r.rc.Routes(),
 	}
 
-	slog.Info(fmt.Sprintf("%s server is now available", r.name))
-
 	return r
 }
 
 func (r *rest) Open() (<-chan bool, error) {
-	slog.Info(fmt.Sprintf("Open %s server...", r.name))
-
 	shutdown := make(chan bool)
 
 	gracefulShutdown := make(chan os.Signal, 1)
@@ -83,14 +75,10 @@ func (r *rest) Open() (<-chan bool, error) {
 		close(shutdown)
 	}()
 
-	slog.Info(fmt.Sprintf("%s is now running!", r.name))
-
 	return shutdown, nil
 }
 
 func (r *rest) Close() error {
-	slog.Info(fmt.Sprintf("Close %s server...", r.name))
-
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
@@ -98,8 +86,6 @@ func (r *rest) Close() error {
 	if err != nil {
 		return err
 	}
-
-	slog.Info("Done closing process!")
 
 	return nil
 }
