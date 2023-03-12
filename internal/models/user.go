b063a90698ecf1b8d64e9d38c0bb3e92706211bf
Hyohwak Lee <crewe1746@naver.com>
2023-03-12 16:10:26 +0900

    Define UserResponse and OtpResponse

--- internal/models/user.go
+++ internal/models/user.go
@@ -16,11 +16,34 @@ type User struct {
 
 type UserPayload struct {
 	ID        string `json:"id"`
-	Email     string `json:"email,omitempty"`
-	DiscordID string `json:"discord_id,omitempty"`
+	Email     string `json:"email"`
+	DiscordID string `json:"discord_id"`
+}
+
+type UserResponse struct {
+	CommonResponse
+	User struct {
+		ID          string    `json:"id"`
+		Email       string    `json:"email"`
+		DiscordID   string    `json:"discord_id"`
+		OtpEnabled  bool      `json:"otp_enabled"`
+		OtpVerified bool      `json:"otp_verified"`
+		CreatedAt   time.Time `json:"created_at"`
+	} `json:"user"`
 }
 
 type OtpPayload struct {
 	ID    string `json:"id"`
 	Token string `json:"token"`
+}
+
+type OtpResponse struct {
+	CommonResponse
+	Otp struct {
+		OtpEnabled  bool   `json:"otp_enabled"`
+		OtpVerified bool   `json:"otp_verified"`
+		OtpValid    bool   `json:"otp_valid,omitempty"`
+		OtpSecret   string `json:"otp_secret,omitempty"`
+		OtpUrl      string `json:"otp_url,omitempty"`
+	} `json:"otp"`
 }
